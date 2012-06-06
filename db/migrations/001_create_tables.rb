72be6c1cc1f0619e07efac0620ce94fd01b54f55
Maxime Bourget <bmx007@gmail.com>
2012-06-06 18:18:51 +0100

    Create db/migrations and use it in test.

---
+++ db/migrations/001_create_tables.rb
@@ -0,0 +1,63 @@
+Sequel.migration do
+  change do
+    create_table :aliquots do
+      primary_key :id
+      String :sample
+      Integer :tag_id
+      Integer :quantity
+      String :type
+    end
+
+    create_table :flowcells do
+      primary_key :id
+    end
+
+    create_table :lanes do
+      #primary_key :flowcell_id, :position
+      primary_key :id
+      Integer :flowcell_id
+      Integer :position
+      Integer :aliquot_id
+    end
+
+    create_table :plates do
+      primary_key :id
+      Integer :row_number
+      Integer :column_number
+    end
+
+    create_table :wells do
+      primary_key :id
+      Integer :plate_id
+      Integer :position
+      Integer :aliquot_id
+    end
+
+    create_table :oligos do
+      primary_key :id
+      String :sequence
+    end
+
+    create_table :tag_groups do
+      primary_key :id
+      String :name
+    end
+
+    create_table :tag_group_associations do
+      primary_key :id
+      Integer :tag_group_id
+      Integer :position
+      Integer :oligo_id
+    end
+
+    create_table :tubes do
+      primary_key :id
+    end
+
+    create_table :tube_aliquots do
+      primary_key :id
+      Integer :tube_id
+      Integer :aliquot_id
+    end
+  end
+end
