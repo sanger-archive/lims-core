e510fcb91e925d096ff5805a8d83c017538e5de7
max <bmx007@gmail.com>
2013-01-21 14:18:45 +0000

    Add pack/unpac_uuid to Labellable persistors

--- lib/lims-core/persistence/sequel/labellable.rb
+++ lib/lims-core/persistence/sequel/labellable.rb
@@ -13,41 +13,65 @@ module Lims::Core
           :labellables
         end
 
+      # Pack if needed an uuid to its store representation
+      # @param [String] uuid
+      # @return [Object]
+      def pack_uuid(uuid)
+        Uuids::UuidResource::pack(uuid)
+      end
+
+      # Unpac if needed an uuid from its store representation
+      # @param [Object] puuid
+      # @return [String]
+      def unpack_uuid(puuid)
+        Uuids::UuidResource::unpack(puuid)
+      end
+
         def save_raw_association(labellables_id, labels_id)
             dataset.insert(:labellables_id => labellables_id, :labels_id  => labels_id)
         end
 
         def filter_attributes_on_save(attributes, *params)
           attributes.delete(:content)
-          name = attributes[:name]
-          attributes[:name] = Uuids::UuidResource::pack(name)
+          if attributes[:type] == "resource"
+            name = attributes[:name]
+            attributes[:name] = pack_uuid(name)
+          end
           attributes
         end
 
-        # Mixin to be included by classes of Labellable::Labels
-        class Label  < Persistence::Labellable::Label
-          include Sequel::Persistor
-
-          def self.table_name
-            :labels
+        def xfilter_attributes_on_load(attributes, *params)
+          if attributes[:type] == "resource"
+            name = attributes[:name]
+            attributes[:name] = unpack_uuid(name)
           end
+          attributes
+        end
 
-          def load(labellable_id)
-            dataset.filter(:labellable_id => labellable_id).each do |att|
-              position = att.delete(:position)
-              label =  Laboratory::Labellable::Label::new(att)
-              yield(position, label)
+          # Mixin to be included by classes of Labellable::Labels
+          class Label  < Persistence::Labellable::Label
+            include Sequel::Persistor
+
+            def self.table_name
+              :labels
             end
-          end
 
-          def filter_attributes_on_save(attributes, labellable_id, position)
-            attributes.tap do
-              attributes[:labellable_id]= labellable_id
-              attributes[:position] = position
+            def load(labellable_id)
+              dataset.filter(:labellable_id => labellable_id).each do |att|
+                position = att.delete(:position)
+                label =  Laboratory::Labellable::Label::new(att)
+                yield(position, label)
+              end
+            end
+
+            def filter_attributes_on_save(attributes, labellable_id, position)
+              attributes.tap do
+                attributes[:labellable_id]= labellable_id
+                attributes[:position] = position
+              end
             end
           end
         end
       end
     end
   end
-end
