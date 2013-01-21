82dd8947954958ab2d96aad30d411cc2d0537669
Loic <llh1@sanger.ac.uk>
2013-01-21 14:19:14 +0000

    [#42646143] Rename well to element

--- lib/lims-core/laboratory/container.rb
+++ lib/lims-core/laboratory/container.rb
@@ -18,10 +18,10 @@ module Lims::Core
         klass.class_eval do
           # each is already defined to look like an Array.
           # This one provide a "String" index (like "A1") aka
-          # well name. It ca be used to access the Plate.
+          # element name. It can be used to access the Container.
           def each_with_index
             @content.each_with_index do |well, index|
-              yield well, index_to_well_name(index)
+              yield well, index_to_element_name(index)
             end
           end
         end
@@ -33,11 +33,11 @@ module Lims::Core
         def [](index)
           case index
           when Array
-            get_well(*index)
+            get_element(*index)
           when /\A([a-zA-Z])(\d+)\z/
             row = $1.ord - ?A.ord
             col = $2.to_i - 1
-            get_well(row, col)
+            get_element(row, col)
             # why not something like
             # get_well(*[$1, $2].zip(%w{A 1}).map { |a,b| [a,b].map(&:ord).inject { |x,y| x-y } })
           when Symbol
@@ -52,55 +52,55 @@ module Lims::Core
 
       # Hash behavior
 
-      # Provides the list of the well names, equivalent to
+      # Provides the list of the element names, equivalent to
       # the Hash#keys methods.
       # @return [Array<String>]
       def keys
-        0.upto(size-1).map { |i| index_to_well_name(i) }
+        0.upto(size-1).map { |i| index_to_element_name(i) }
       end
 
-      # List of the wells, equivalent to Hash#values
+      # List of the elements, equivalent to Hash#values
       def values
         @content
       end
 
-      def indexes_to_well_name(row, column)
-        self.class.indexes_to_well_name(row, column)
+      def indexes_to_element_name(row, column)
+        self.class.indexes_to_element_name(row, column)
       end
 
       # Convert an index to String
       # @param [Fixnum] index (stating at 0)
       # @return [String] ex "A1"
       # @todo memoize if needed
-      def index_to_well_name(index)
+      def index_to_element_name(index)
         row = index / number_of_columns
         column = index % number_of_columns
 
-        indexes_to_well_name(row, column)
+        indexes_to_element_name(row, column)
       end
 
-      # Convert a well name to a index (number fro 0 to size -1)
-      def well_name_to_index(name)
+      # Convert a element name to a index (number fro 0 to size -1)
+      def element_name_to_index(name)
         raise NotImplementedError
       end
 
-      # return a well from a 2D index
+      # return a element from a 2D index
       # Also check the boundary
       # @param [Fixnum] row index of the row (starting at 0)
       # @param [Fixnum] col index of the column (starting at 0)
-      # @return [Well]
-      def get_well(row, col)
+      # @return [Object]
+      def get_element(row, col)
         raise IndexOutOfRangeError unless (0...number_of_rows).include?(row)
         raise IndexOutOfRangeError unless (0...number_of_columns).include?(col)
         @content[row*number_of_columns + col]
       end
-      private :get_well
+      private :get_element
 
 
       module ClassMethods
-        def indexes_to_well_name(row, column)
+        def indexes_to_element_name(row, column)
           "#{(row+?A.ord).chr}#{column+1}"
-        end
+        end 
       end
     end
   end
