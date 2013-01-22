281016b6d60eb467951ac073e6c575cd9456db37
Loic <llh1@sanger.ac.uk>
2013-01-22 10:04:31 +0000

    [#42646511] Change tubes attribute name to tubes_description

--- lib/lims-core/actions/create_tube_rack.rb
+++ lib/lims-core/actions/create_tube_rack.rb
@@ -11,16 +11,16 @@ module Lims::Core
         attribute :"number_of_#{w}s", Fixnum, :required => true, :gte => 0, :writer => :private
       end
 
-      # @attribute [Hash<String, Laboratory::Tube>] tubes
+      # @attribute [Hash<String, Laboratory::Tube>] tubes description
       # @example
       # {"A1" => tube_1, "B4" => tube_2}
-      attribute :tubes, Hash, :default => {}
+      attribute :tubes_description, Hash, :default => {}
 
       def _call_in_session(session)
         tube_rack = Laboratory::TubeRack.new(:number_of_columns => number_of_columns, :number_of_rows => number_of_rows)
         session << tube_rack
 
-        tubes.each do |position, tube|
+        tubes_description.each do |position, tube|
           tube_rack[position] = tube
         end
 
--- spec/actions/create_tube_rack_spec.rb
+++ spec/actions/create_tube_rack_spec.rb
@@ -29,7 +29,7 @@ module Lims::Core
     end
 
     shared_context "for a tube rack with tubes" do
-      let(:tubes) {{
+      let(:tubes_description) {{
         "A1" => Laboratory::Tube.new,
         "B3" => Laboratory::Tube.new,
         "D5" => Laboratory::Tube.new,
@@ -39,13 +39,13 @@ module Lims::Core
       subject do
         CreateTubeRack.new(:store => store, :user => user, :application => application) do |a,s|
           a.ostruct_update(dimensions)
-          a.tubes = tubes
+          a.tubes_description = tubes_description
         end
       end
 
       let(:tube_rack_checker) do
         lambda do |tube_rack|
-          tubes.each do |position, tube|
+          tubes_description.each do |position, tube|
             tube_rack[position].should == tube
           end
         end
