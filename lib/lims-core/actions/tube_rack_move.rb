6169c141ff38b38c0fe702ee68d737bf525a5552
Loic <llh1@sanger.ac.uk>
2013-01-23 10:47:33 +0000

    [#42646887] Add comments, remove useless custom exception.

--- lib/lims-core/actions/tube_rack_move.rb
+++ lib/lims-core/actions/tube_rack_move.rb
@@ -4,6 +4,8 @@ require 'lims/core/laboratory/tube_rack'
 
 module Lims::Core
   module Actions
+    # This action moves physically tubes from a source rack
+    # to a target rack according to a transfer map.
     class TubeRackMove
       include Action
 
@@ -11,14 +13,13 @@ module Lims::Core
       attribute :target, Laboratory::TubeRack, :required => true, :writer => :private
       attribute :move_map, Hash, :required => true, :writer => :private
 
-
-      class TubePresentInTargetLocation < StandardError
-      end
-
+      # Move tubes from a source tube rack to a target
+      # tube rack. If a tube is already present in the
+      # target location, a RackPositionNotEmpty exception
+      # is raised in the Laboratory::TubeRack class.
+      # The tube is removed from the source tube rack.
       def _call_in_session(session)
         move_map.each do |from, to|
-          raise TubePresentInTargetLocation, "#{to} location is not empty" unless target[to].nil?
-
           unless source[from].nil?
             target[to] = source[from]
             source[from] = nil
