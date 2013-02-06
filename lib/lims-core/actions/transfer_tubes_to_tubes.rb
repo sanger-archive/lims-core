cecc8d0c071d410e77883ea883639fc049a684fe
Karel <ke4@sanger.ac.uk>
2013-02-06 22:59:14 +0000

    [#43848717] Added transfer tubes to tubes action

---
+++ lib/lims-core/actions/transfer_tubes_to_tubes.rb
@@ -0,0 +1,59 @@
+require 'lims/core/actions/action'
+
+require 'lims/core/laboratory/spin_column'
+require 'lims/core/laboratory/tube'
+
+module Lims::Core
+  module Actions
+
+    # This {Action} transfers the given fraction and type of aliquot from tube-like asset(s)
+    # to tube-like asset(s).
+    # It takes an array, which contains transfer elements. 
+    # An element has a source, target, amount, fraction and type parameter.
+    # Source and targets are tube-like assets (a tube or spin column).
+    # Amount is an amount of an aliquot to transfer.
+    # Fraction is the fraction of an aliquot to transfer.
+    # Type is the type of the aliquot.
+    class TransferTubesToTubes
+      include Action
+
+      attribute :transfers, Array, :required => true, :writer => :private
+
+      def _validate_parameters
+        raise ArgumentError, "The transfer array should not be null." unless transfers
+        raise ArgumentError, "You should give the fraction OR the amount of the transfer, not both." unless validAmountAndFraction
+      end
+
+      def validAmountAndFraction
+        valid = true
+        transfers.each do |transfer|
+          if (transfer["fraction"].nil? && transfer["amount"].nil?) || (transfer["fraction"] && transfer["amount"])
+            valid = false
+            break
+          end
+        end
+        valid
+      end
+
+      # transfer the given fraction of aliquot from tube-like asset(s)
+      # to tube-like asset(s)
+      def _call_in_session(session)
+        transfers.each do |transfer|
+          next unless transfer["fraction"]
+          amount = transfer["source"].quantity * transfer["fraction"]
+          transfer["amount"] = amount
+        end
+
+        transfers.each do |transfer|
+          transfer["target"] << transfer["source"].take_amount(transfer["amount"])
+
+          unless transfer["aliquot_type"].nil?
+            transfer["target"].each do |aliquot|
+              aliquot.type = transfer["aliquot_type"]
+            end
+          end
+        end
+      end
+    end
+  end
+end
