ede2c464c9e88130b717fdb8694013cf897502ec
Loic <llh1@sanger.ac.uk>
2013-02-12 16:46:03 +0000

    [#44287125] Add type and max_volume attributes for tube.

--- lib/lims-core/laboratory/tube.rb
+++ lib/lims-core/laboratory/tube.rb
@@ -10,9 +10,9 @@ module Lims::Core
       include Resource
       include Receptacle
       # Type contains the actual type of the tube, for example Eppendorf.
-      attribute :type, String, :required => false, :writer => :private
-      # Store the maximum volume a tube can hold.
-      attribute :volume_max, Numeric, :required => false, :gte => 0, :writer => :private
+      attribute :type, String, :required => false
+      # Store the maximum volume a tube can hold in ml.
+      attribute :max_volume, Numeric, :gte => 0, :required => false
     end
   end
 end
