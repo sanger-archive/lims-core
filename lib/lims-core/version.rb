4cb831965117a30f48192c67e98bcc088bc0aa6c
max <bmx007@gmail.com>
2013-02-13 16:14:26 +0000

    bump 'merge friendly\ version 1.2.0.5.1

--- lib/lims-core/version.rb
+++ lib/lims-core/version.rb
@@ -1,5 +1,31 @@
 module Lims
   module Core
-    VERSION = "1.2.0.5.1"
+    # Test a way to increment version in a merged-friendly way
+    # Each user need to add an x, in its appropriate section (this will
+    # avoid collision)
+    # and clear eventually the minor version of everybody .
+    # Please leave the marker of other developper
+    #
+    MINOR_DEV = %{
+    --llh1
+    --ke4
+    --mb14
+    x
+
+  }
+    MAJOR_DEV = %{
+    --llh1
+    --ke4
+    --mb14
+  x
+  x
+  x
+  x
+  x
+
+}
+
+
+    VERSION = "1.2.0.#{MAJOR_DEV.scan(/\sx/i).size}.#{MINOR_DEV.scan(/\sx/i).size}"
   end
 end
