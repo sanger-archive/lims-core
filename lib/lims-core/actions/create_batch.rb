a321c0dbeccea59e9424945a71a454721c4afbfc
Loic <llh1@sanger.ac.uk>
2013-02-14 09:27:09 +0000

    [fixes #44386853] Create action for batch and write specs

---
+++ lib/lims-core/actions/create_batch.rb
@@ -0,0 +1,23 @@
+# vi: ts=2:sts=2:et:sw=2  spell:spelllang=en  
+require 'lims/core/actions/action'
+require 'lims/core/organization/batch'
+
+module Lims::Core
+  module Actions
+    class CreateBatch
+      include Action
+
+      def _call_in_session(session)
+        batch = Organization::Batch.new
+        session << batch
+        {:batch => batch, :uuid => session.uuid_for!(batch)}
+      end
+
+    end
+  end
+  module Organization
+    class Batch
+      Create = Actions::CreateBatch
+    end
+  end
+end
---
+++ spec/actions/create_batch_spec.rb
@@ -0,0 +1,37 @@
+# Spec requirements
+require 'actions/spec_helper'
+require 'actions/action_examples'
+
+#Model requirements
+require 'lims/core/actions/create_batch'
+require 'lims/core/persistence/store'
+
+module Lims::Core
+  module Actions
+    describe CreateBatch do
+      context "with a valid store" do
+        include_context "create object"
+        let (:store) { Persistence::Store.new }
+        let(:user) { mock(:user) }
+        let(:application) { "Test create batch" }
+
+        context "create a batch" do
+          subject do
+            described_class.new(:store => store, :user => user, :application => application)  do |a,s|
+            end
+          end 
+
+          it_behaves_like "an action"
+
+          it "create a batch when called" do
+            Persistence::Session.any_instance.should_receive(:save)
+            result = subject.call
+            result.should be_a(Hash)
+            result[:batch].should be_a(Organization::Batch)
+            result[:uuid].should == uuid
+          end
+        end
+      end
+    end
+  end
+end
