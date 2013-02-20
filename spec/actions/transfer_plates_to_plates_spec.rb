886a6c8afebeddcd6caa8c6252bf5be830accfa3
Karel <ke4@sanger.ac.uk>
2013-02-20 11:08:56 +0000

    [fixes #44633429] fraction/amount to TransferPlatesToPlates

--- spec/actions/transfer_plates_to_plates_spec.rb
+++ spec/actions/transfer_plates_to_plates_spec.rb
@@ -7,18 +7,6 @@ require 'laboratory/tube_rack_shared'
 # Model requirements
 require 'lims/core/actions/transfer_plates_to_plates'
 
-# check whether the aliquot quantity is zero in the element (well or window)
-def zero_aliquot_quantity?(w)
-  valid = true
-  w.each do |aliquot|
-    if aliquot.quantity != 0
-      valid = false
-      break;
-    end
-  end
-  valid
-end
-
 shared_examples_for "transfer from many plates to many gels" do
   it "transfers the contents of plate-like(s) to plate-like(s)" do
     subject.call
@@ -26,29 +14,32 @@ shared_examples_for "transfer from many plates to many gels" do
       plate1, plate2 = [plate1_id, plate2_id].map { |id| session.plate[id] }
       gel1, gel2 = [gel1_id, gel2_id].map { |id| session.gel[id] }
 
-      # I think, it should be as simple as this:
-      # plate1["A1"].should be_nil
-      # but the aliquot implementation is just setting the quantity to 0
-      zero_aliquot_quantity?(plate1["A1"]).should == true
-      zero_aliquot_quantity?(plate1["C3"]).should == true
+      plate1["A1"].quantity.should == final_quantity_plate1_A1
+      plate1["C3"].quantity.should == final_quantity_plate1_C3
 
       gel1["B2"].should_not be_nil
       gel1["E8"].should_not be_nil
-      gel1.each do |window|
-        window.each do |aliquot|
-          aliquot.type.should == type1
-        end
+
+      gel1["B2"].each do |aliquot|
+        aliquot.type.should == type1
       end
 
-      zero_aliquot_quantity?(plate2["A1"]).should == true
-      zero_aliquot_quantity?(plate2["C3"]).should == true
+      gel1["E8"].each do |aliquot|
+        aliquot.type.should == type1
+      end
+
+      plate2["A1"].quantity.should == final_quantity_plate2_A1
+      plate2["C3"].quantity.should == final_quantity_plate2_C3
 
       gel2["B2"].should_not be_nil
       gel2["E8"].should_not be_nil
-      gel2.each do |window|
-        window.each do |aliquot|
-          aliquot.type.should == type2
-        end
+
+      gel2["B2"].each do |aliquot|
+        aliquot.type.should == type2
+      end
+
+      gel2["E8"].each do |aliquot|
+        aliquot.type.should == type2
       end
     end
   end
@@ -61,29 +52,34 @@ shared_examples_for "transfer from many racks to many plates" do
       rack1, rack2 = [rack1_id, rack2_id].map { |id| session.tube_rack[id] }
       plate1, plate2 = [plate1_id, plate2_id].map { |id| session.plate[id] }
 
-      # I think, it should be as simple as this:
-      # rack1["A1"].should be_nil
-      # but the aliquot implementation is just setting the quantity to 0
-      zero_aliquot_quantity?(rack1["A1"]).should == true
-      zero_aliquot_quantity?(rack1["C3"]).should == true
-
+      rack1["A1"].quantity.should == final_quantity_rack1_A1
+      rack1["C3"].quantity.should == final_quantity_rack1_C3
       plate1["B2"].should_not be_nil
       plate1["E8"].should_not be_nil
-      plate1.each do |window|
-        window.each do |aliquot|
-          aliquot.type.should == type1
-        end
+
+      plate1["B2"].each do |aliquot|
+        aliquot.type.should == type1
+        aliquot.quantity.should == final_quantity_plate1_B2
       end
 
-      zero_aliquot_quantity?(rack2["A1"]).should == true
-      zero_aliquot_quantity?(rack2["C3"]).should == true
+      plate1["E8"].each do |aliquot|
+        aliquot.type.should == type1
+        aliquot.quantity.should == final_quantity_plate1_E8
+      end
 
+      rack2["A1"].quantity.should == final_quantity_rack2_A1
+      rack2["C3"].quantity.should == final_quantity_rack2_C3
       plate2["B2"].should_not be_nil
       plate2["E8"].should_not be_nil
-      plate2.each do |window|
-        window.each do |aliquot|
-          aliquot.type.should == type2
-        end
+
+      plate2["B2"].each do |aliquot|
+        aliquot.type.should == type2
+        aliquot.quantity.should == final_quantity_plate2_B2
+      end
+
+      plate2["E8"].each do |aliquot|
+        aliquot.type.should == type2
+        aliquot.quantity.should == final_quantity_plate2_E8
       end
     end
   end
@@ -105,11 +101,21 @@ module Lims::Core
           let(:number_of_columns) { 12 }
 
           context "with valid parameters" do
-            let(:type1) { "NA" }
+            let(:type1) { "RNA" }
             let(:type2) { "DNA" }
             context "transfer from 2 plates to 2 gels" do
-              let(:plate1_id) { save(new_plate_with_samples(5, 100)) }
-              let(:plate2_id) { save(new_plate_with_samples(5, 100)) }
+              let(:quantity1) { 100 }
+              let(:quantity2) { 100 }
+              let(:final_quantity_plate1_A1) { 40 }
+              let(:final_quantity_plate1_C3) { 40 }
+              let(:final_quantity_plate2_A1) { 70 }
+              let(:final_quantity_plate2_C3) { 70 }
+              let(:final_quantity_gel1_B2) { 60 }
+              let(:final_quantity_gel1_E8) { 60 }
+              let(:final_quantity_gel2_B2) { 30 }
+              let(:final_quantity_gel2_E8) { 30 }
+              let(:plate1_id) { save(new_plate_with_samples(5, quantity1)) }
+              let(:plate2_id) { save(new_plate_with_samples(5, quantity2)) }
               let(:gel1_id) { save(new_empty_gel) }
               let(:gel2_id) { save(new_empty_gel) }
 
@@ -120,13 +126,29 @@ module Lims::Core
                 gel1, gel2 = [gel1_id, gel2_id].map { |id| session.gel[id] }
 
                 action.transfers = [ { "source" => plate1,
+                                       "source_location" => "A1",
                                        "target" => gel1,
-                                       "transfer_map" => { "A1" => "B2", "C3" => "E8" },
+                                       "target_location" => "B2",
+                                       "fraction" => 0.6,
+                                       "aliquot_type" => type1},
+                                     { "source" => plate1,
+                                       "source_location" => "C3",
+                                       "target" => gel1,
+                                       "target_location" => "E8",
+                                       "fraction" => 0.6,
                                        "aliquot_type" => type1},
                                      { "source" => plate2,
+                                       "source_location" => "A1",
                                        "target" => gel2,
-                                       "transfer_map" => { "A1" => "B2", "C3" => "E8" },
-                                       "aliquot_type" => type2}
+                                       "target_location" => "B2",
+                                       "fraction" => 0.3,
+                                       "aliquot_type" => type2},
+                                     { "source" => plate2,
+                                       "source_location" => "C3",
+                                       "target" => gel2,
+                                       "target_location" => "E8",
+                                       "fraction" => 0.3,
+                                       "aliquot_type" => type2},
                 ]
               end
               }
@@ -134,9 +156,19 @@ module Lims::Core
               it_behaves_like "transfer from many plates to many gels"
             end
 
-            context "transfer from 2 racks to 2 plates" do
-              let(:rack1_id) { save(new_tube_rack_with_samples(5, 100)) }
-              let(:rack2_id) { save(new_tube_rack_with_samples(5, 100)) }
+            context "transfer from 2 racks to 2 plates", :focus => true do
+              let(:quantity1) { 1000 }
+              let(:quantity2) { 1000 }
+              let(:final_quantity_rack1_A1) { 940 }
+              let(:final_quantity_rack1_C3) { 940 }
+              let(:final_quantity_rack2_A1) { 970 }
+              let(:final_quantity_rack2_C3) { 970 }
+              let(:final_quantity_plate1_B2) { 60 }
+              let(:final_quantity_plate1_E8) { 60 }
+              let(:final_quantity_plate2_B2) { 30 }
+              let(:final_quantity_plate2_E8) { 30 }
+              let(:rack1_id) { save(new_tube_rack_with_samples(5, quantity1, 1000)) }
+              let(:rack2_id) { save(new_tube_rack_with_samples(5, quantity2, 1000)) }
               let(:plate1_id) { save(new_empty_plate) }
               let(:plate2_id) { save(new_empty_plate) }
         
@@ -145,14 +177,30 @@ module Lims::Core
                                             :application => application) do |action, session|
                 rack1, rack2 = [rack1_id, rack2_id].map { |id| session.tube_rack[id] }
                 plate1, plate2 = [plate1_id, plate2_id].map { |id| session.plate[id] }
-        
-                action.transfers = [ { "source" => rack1,
+
+                  action.transfers = [ { "source" => rack1,
+                                       "source_location" => "A1",
                                        "target" => plate1,
-                                       "transfer_map" => { "A1" => "B2", "C3" => "E8" },
+                                       "target_location" => "B2",
+                                       "amount" => 60,
+                                       "aliquot_type" => type1},
+                                     { "source" => rack1,
+                                       "source_location" => "C3",
+                                       "target" => plate1,
+                                       "target_location" => "E8",
+                                       "amount" => 60,
                                        "aliquot_type" => type1},
                                      { "source" => rack2,
+                                       "source_location" => "A1",
                                        "target" => plate2,
-                                       "transfer_map" => { "A1" => "B2", "C3" => "E8" },
+                                       "target_location" => "B2",
+                                       "amount" => 30,
+                                       "aliquot_type" => type2},
+                                     { "source" => rack2,
+                                       "source_location" => "C3",
+                                       "target" => plate2,
+                                       "target_location" => "E8",
+                                       "amount" => 30,
                                        "aliquot_type" => type2}
                 ]
               end
