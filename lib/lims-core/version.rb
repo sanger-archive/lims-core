1498aed64b4303da4ff361dda488e107e6449e7b
Karel <ke4@sanger.ac.uk>
2013-02-26 12:18:15 +0000

    [fixes #45136133] Added multiply tube rack move action
    Refactored the tube rack move action to use
    multiply source and target laboratory asset.

--- lib/lims-core/version.rb
+++ lib/lims-core/version.rb
@@ -9,17 +9,14 @@ module Lims
     MINOR_DEV = %{
     --llh1
     --ke4
-  x
-    x
-    x
     --mb14
-    x
 
   }
     MAJOR_DEV = %{
     --llh1
     --ke4
-  x
+    x
+    x
     --mb14
 
 }
--- spec/actions/tube_rack_move_spec.rb
+++ spec/actions/tube_rack_move_spec.rb
@@ -7,22 +7,18 @@ require 'laboratory/tube_rack_shared'
 require 'lims/core/actions/tube_rack_move'
 require 'lims/core/persistence/sequel/store'
 
-
 module Lims::Core
   module Actions
     describe TubeRackMove do
       context "with a sequel store" do
         include_context "for application", "test tube rack move"
-        include_context "prepare tables"
+        include_context "sequel store"
         include_context "tube_rack factory"
 
         let(:number_of_rows) { 8 }
         let(:number_of_columns) { 12 }
-        let(:db) { ::Sequel.sqlite('') }
-        let(:store) { Persistence::Sequel::Store.new(db) }
-        before(:each) { prepare_table(db) }
 
-        let(:source_id) {
+        let(:source1_id) {
           store.with_session do |session|
             rack = new_tube_rack_with_samples(1)
             session << rack
@@ -30,9 +26,25 @@ module Lims::Core
           end.call
         }
 
-        let(:target_id) {
+        let(:source2_id) {
           store.with_session do |session|
-            rack = target_tube_rack 
+            rack = new_tube_rack_with_samples(1)
+            session << rack
+            lambda { session.tube_rack.id_for(rack) }
+          end.call
+        }
+
+        let(:target1_id) {
+          store.with_session do |session|
+            rack = target_tube_rack1
+            session << rack
+            lambda { session.tube_rack.id_for(rack) }
+          end.call
+        }
+
+        let(:target2_id) {
+          store.with_session do |session|
+            rack = target_tube_rack2
             session << rack
             lambda { session.tube_rack.id_for(rack) }
           end.call
@@ -47,13 +59,16 @@ module Lims::Core
 
         # Tube already present in target tube rack
         context "invalid transfer" do
-          let(:target_tube_rack) { new_tube_rack_with_samples(1) }
+          let(:target_tube_rack1) { new_tube_rack_with_samples(1) }
 
           subject do
-            described_class.new(:store => store, :user => user, :application => application) do |a,s|
-              a.source = s.tube_rack[source_id]
-              a.target = s.tube_rack[target_id]
-              a.move_map = {:A4 => :E9}
+            described_class.new(:store => store, :user => user, :application => application) do |action, session|
+              action.moves = [
+                {"source" => session.tube_rack[source1_id],
+                 "source_location" => "A4",
+                 "target" => session.tube_rack[target1_id],
+                 "target_location" => "E9" }
+              ]
             end
           end
 
@@ -63,23 +78,49 @@ module Lims::Core
         end
 
         context "valid transfer" do
-          let(:target_tube_rack) { new_empty_tube_rack }
+          let(:target_tube_rack1) { new_empty_tube_rack }
+          let(:target_tube_rack2) { new_empty_tube_rack }
           before(:each) { subject.call }
           subject do
-            described_class.new(:store => store, :user => user, :application => application) do |a,s|
-              a.source = s.tube_rack[source_id]
-              a.target = s.tube_rack[target_id]
-              a.move_map = {:A4 => :E10}
+            described_class.new(:store => store, :user => user, :application => application) do |action,session|
+              source_tube_rack1, source_tube_rack2 = [source1_id, source2_id].map { |uuid| session.tube_rack[uuid] }
+              action.moves = [
+                {"source" => source_tube_rack1,
+                 "source_location" => "A1",
+                 "target" => target_tube_rack1,
+                 "target_location" => "B9" },
+                {"source" => source_tube_rack1,
+                 "source_location" => "B2",
+                 "target" => target_tube_rack2,
+                 "target_location" => "F3" },
+                {"source" => source_tube_rack2,
+                 "source_location" => "C5",
+                 "target" => target_tube_rack1,
+                 "target_location" => "D4" },
+                {"source" => source_tube_rack2,
+                 "source_location" => "E8",
+                 "target" => target_tube_rack2,
+                 "target_location" => "A9" }
+              ]
             end
           end
 
           it "saves the transfered rack" do
             store.with_session do |session|
-              source = session.tube_rack[source_id]
-              target = session.tube_rack[target_id]
-              source[:A4].should be_nil
-              target[:E10].should_not be_nil
-              target[:E10].should be_a(Lims::Core::Laboratory::Tube)
+              source_tube_rack1, source_tube_rack2 = [source1_id, source2_id].map { |uuid| session.tube_rack[uuid] }
+              source_tube_rack1[:A1].should be_nil
+              source_tube_rack1[:B2].should be_nil
+              source_tube_rack2[:C5].should be_nil
+              source_tube_rack2[:E8].should be_nil
+
+              target_tube_rack1[:B9].should_not be_nil
+              target_tube_rack1[:B9].should be_a(Lims::Core::Laboratory::Tube)
+              target_tube_rack1[:D4].should_not be_nil
+              target_tube_rack1[:D4].should be_a(Lims::Core::Laboratory::Tube)
+              target_tube_rack2[:F3].should_not be_nil
+              target_tube_rack2[:F3].should be_a(Lims::Core::Laboratory::Tube)
+              target_tube_rack2[:A9].should_not be_nil
+              target_tube_rack2[:A9].should be_a(Lims::Core::Laboratory::Tube)
             end
           end
         end
