5308ad6fece63f097b28203131ea653e992badc4
Karel <ke4@sanger.ac.uk>
2013-03-22 14:55:11 +0000

    [fix 46712391] do not autoload unnecessary resources

--- lib/lims-core/laboratory.rb
+++ lib/lims-core/laboratory.rb
@@ -1,12 +1,13 @@
 #vi: ts=2 sw=2 et
-require 'lims/core/laboratory/plate.rb'
-require 'lims/core/laboratory/tube.rb'
-require 'lims/core/laboratory/flowcell.rb'
+unless defined?(Lims::Core::NO_AUTOLOAD)
+  require 'lims/core/laboratory/plate.rb'
+  require 'lims/core/laboratory/tube.rb'
+  require 'lims/core/laboratory/flowcell.rb'
 
-require 'lims/core/laboratory/aliquot'
-require 'lims/core/laboratory/sample'
-require 'lims/core/laboratory/oligo'
-
+  require 'lims/core/laboratory/aliquot'
+  require 'lims/core/laboratory/sample'
+  require 'lims/core/laboratory/oligo'
+end
 module Lims::Core
   # Things used/found in the lab. Includes pure laboratory (inert things as {Plate plates}, {Tube tubes})
   # and chemical one (as {Aliquot aliquots}, {Sample samples}).
