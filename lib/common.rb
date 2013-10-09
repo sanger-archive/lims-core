7b0ed73d99f7d16e77bef336683e337bb7555d2a
Karel <ke4@sanger.ac.uk>
2013-10-09 11:56:55 +0100

    moved gem_available method to Lims::Core namespace

--- lib/common.rb
+++ lib/common.rb
@@ -7,6 +7,27 @@ require 'facets/array'
 require 'virtus'
 require 'aequitas/virtus_integration'
 
+module Lims::Core
+  def self.gem_available?(gemfile)
+    begin
+      Gem::Specification.find_by_name(gemfile)
+      true
+    rescue Gem::LoadError
+      false
+    end
+  end
+end
+
+case
+when Lims::Core.gem_available?('oj')
+  require 'oj'
+when Lims::Core.gem_available?('jrjackson')
+  require 'jrjackson'
+else
+  require 'json'
+  alias :orig_to_json :to_json
+end
+
 class Object
   def andtap(&block)
     self && (block ? block[self] : self)
@@ -16,33 +37,14 @@ class Object
     @__parent_scope ||= eval self.name.split('::').tap { |_| _.pop }.join('::')
   end
 
-  def gem_available?(gemfile)
-    begin
-      Gem::Specification.find_by_name(gemfile)
-      true
-    rescue Gem::LoadError
-      false
-    end
-  end
-
-case
-when gem_available?('oj')
-  require 'oj'
-when gem_available?('jrjackson')
-  require 'jrjackson'
-else
-  require 'json'
-  alias_method :orig_to_json, :to_json
-end
-
   def to_json(object)
     case
-    when gem_available?('oj')
+    when Lims::Core.gem_available?('oj')
       Oj.dump(object)
-    when gem_available?('jrjackson')
+    when Lims::Core.gem_available?('jrjackson')
       JrJackson::Json.dump(object)
     else
-      object.to_json
+      object.orig_to_json
     end
   end
 end
