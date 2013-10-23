db6f3026c5e2c4c4352f42b85fbad6699603e9e7
Loic <llh1@sanger.ac.uk>
2013-10-23 11:39:35 +0100

    Add spec for decoding json

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -10,11 +10,16 @@ module Lims::Core
       end
 
       let(:current_time) { Time.now.utc }
-      let(:ruby_hash) { {:a => {:b => {:date => current_time}}} }
+      let(:ruby_hash) { {"a" => {"b" => {"date" => current_time}}} }
       let(:expected_json) { "{\"a\":{\"b\":{\"date\":\"#{current_time}\"}}}" }
+      let(:expected_ruby_hash) { {"a" => {"b" => {"date" => current_time.to_s}}} }
 
       it "encodes correctly the hash into json" do
         Helpers::to_json(ruby_hash).should == expected_json
+      end
+
+      it "decodes correctly the json into ruby hash" do
+        Helpers::load_json(expected_json).should == expected_ruby_hash
       end
     end
   end
