9a61c461287775ad8c33787132f3cd4655445cb2
Loic <llh1@sanger.ac.uk>
2013-11-26 12:48:07 +0000

    Remove default behaviour, just define the namespace

--- lib/lims-core/persistence/persistor_module.rb
+++ lib/lims-core/persistence/persistor_module.rb
@@ -1,29 +1,7 @@
 module Lims::Core
   module Persistence
-    module PersistorModule
-      module BasePersistorModule
-
-        # @param [String] model
-        # @return [Bool]
-        # Return true if the persistor module should extend the <model> persistor instance
-        def self.defined_for?(model)
-          true
-        end
-
-        # @param [StateGroup] states
-        # @param [Array] params
-        # Call each persistor modules which extend the persistor instance.
-        # The called method is the method formed by the persistor module name
-        # in snake case.
-        # @example: PersistorModule::DoSomethingToThePersistor -> do_something_to_the_persistor
-        def call_persistor_modules(states, *params)
-          Persistence::Persistor::persistor_module_map[Session.model_to_name(model)].map do |module_name|
-            module_name.to_s.split("::").last.gsub(/(.)([A-Z])/, '\1_\2').downcase
-          end.each do |method_name|
-            self.send(method_name, states, *params) if self.respond_to?(method_name)
-          end
-        end
-      end
+    module PersistenceModule
+      # Persistor modules
     end
   end
 end
