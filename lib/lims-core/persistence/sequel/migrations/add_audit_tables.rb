077ad7ed160746466bab784b8594fd929e2b86f5
max <bmx007@gmail.com>
2013-12-12 16:53:00 +0000

    Creating revision tables and tables

---
+++ lib/lims-core/persistence/sequel/migrations/add_audit_tables.rb
@@ -0,0 +1,96 @@
+module Lims::Core::Persistence::Sequel::Migrations
+  module AddAuditTables
+    def self.migration(exclude_tables={})
+      exclude_tables[:schema_info] = true
+      this = self
+      Proc.new do
+        change do
+          # Create session table
+          create_table :sessions do
+            primary_key :id
+            String :user
+            String :application
+            timestamp :start_time
+            DateTime :end_time
+          end
+
+          # Initial session id
+          session_id = 1
+          #create migration session
+          self << <<-EOS
+          INSERT INTO sessions(id, user, application)
+          VALUES(#{session_id}, 'admin', 'initial migration');
+          EOS
+          DB.tables.each do |table_name|
+            table = DB[table_name]
+            next if exclude_tables.include?(table_name)  
+
+            # extend all tables with revision_id
+            if DB[table].columns.include?(:revision)  == false
+              alter_table table_name do             
+                add_column :revision,  Integer, :default => 1
+              end
+            end
+
+
+            # create history table
+            revision_table = "#{table_name}_revision"
+            self << <<-EOS
+            CREATE TABLE #{revision_table} AS
+            SELECT *, 'initial' AS `action`, #{session_id} as session_id
+            FROM #{table_name}
+            EOS
+
+            puts "adding key to #{revision_table}"
+            alter_table revision_table do
+              add_primary_key :internal_id
+              add_index [:id, :revision], :unique => true
+              add_foreign_key [:session_id], :sessions, :key => :id
+            end
+
+
+            # Create trigger              
+            trigger_name = "maintain_#{table_name}_on_insert"
+            self  << "DROP TRIGGER IF EXISTS #{trigger_name};"
+            trigger_code = <<-EOT
+
+            CREATE TRIGGER #{trigger_name}  AFTER INSERT ON  #{table_name}
+            FOR EACH ROW
+            BEGIN
+            #{this.insert_into_revision(table, revision_table)}
+            EOT
+
+            puts trigger_code
+            self << trigger_code
+
+            trigger_name = "maintain_#{table_name}_on_update"
+            self  << "DROP TRIGGER IF EXISTS #{trigger_name};"
+            trigger_code = <<-EOT
+
+            CREATE TRIGGER #{trigger_name}  BEFORE UPDATE ON  #{table_name}
+            FOR EACH ROW
+            BEGIN
+            # Update the revision number
+            SET NEW.revision = NEW.revision+1;
+            # Update the revision table
+            #{this.insert_into_revision(table, revision_table)}
+            EOT
+
+            puts trigger_code
+            self << trigger_code
+          end
+          raise "Failed so we can test it over and over"
+        end
+      end
+    end
+
+    def self.insert_into_revision(table, revision_table)
+      %Q{ INSERT INTO #{revision_table}                                 
+            SET #{table.columns.map { |c| "`#{c}` = NEW.#{c}" }.join(', ')},
+            `action` = 'insert',
+            `session_id` = @session_id;
+            END;
+          }
+    end
+  end
+end
